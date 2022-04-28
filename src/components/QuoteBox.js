57b521b593944e27cd8c86c1ea974c06dc53a7fd
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-28 20:54:40 -0300

    card

---
+++ src/components/QuoteBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const QuoteBox = () => {
+    return (
+        <div>
+            <div className="card">
+                <div className="text">
+                    <p>"Life isn’t about getting and having, it’s about giving and being."</p>
+                    <div className="autor"><h4>"Kevin Kruse"</h4></div>
+                    <div className="button"><button>Click</button>   </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default QuoteBox;
