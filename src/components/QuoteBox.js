997ce80af9b78a3d079fa16f85c5977ec19ddc3b
Diego Bravo <dbravo_2015@hotmail.com>
2022-04-29 02:44:31 -0300

    final 1.0

--- src/components/QuoteBox.js
+++ src/components/QuoteBox.js
@@ -1,16 +1,31 @@
 import React, { useState } from 'react';
 import quotebox from '../quotebox'
 
+
 const colors=[
-  
-    "blue"	,   
-    "blueviolet",	
-   " brown"	,
-    "yellow",	 	
-    "chocolate"	 ,
-    "coral	",
-    	 
-  	]
+ "aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow",
+"aquamarine","yellow","chocolate","coral", "aquamarine","yellow","chocolate","coral","green","blueviolet","greenyellow"
+]
 
 const QuoteBox = () => {
     const randomquote = Math.floor(Math.random() * quotebox.length);
@@ -26,11 +41,12 @@ const randomcolors = Math.floor(Math.random() * quotebox.length);
 
     return (
         <div className="card" style={{background: colors[randomcolors]}} >
-            <div   style={{background: colors[randomcolors]}} >
+            <div  >
                 <div className="text">
-                    <p>"{quotebox[next].quote}"</p>
-                    <div className="autor"><h4>"{quotebox[next].author}"</h4></div>
-                    <div ><button className="button" onClick={ nextPhrase}  >click</button> </div>
+                    <p>{quotebox[next].quote}</p>
+                    <div className="autor"><h4>{quotebox[next].author}</h4></div>
+                    <div className="button" ><button  onClick={ nextPhrase} ><i class="fa-solid fa-circle-chevron-right"></i></button> </div>
+                                    
                 </div>
             </div>
         </div>
